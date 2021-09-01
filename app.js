69a597b549dbf753a48a40752b0b488027b1857a
JSaturn9 <69169169+JSaturn9@users.noreply.github.com>
2021-09-01 16:22:55 -0400

    added emailjs

--- app.js
+++ app.js
@@ -4,4 +4,30 @@ const menuLinks = document.querySelector('.navbar_menu');
 menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
-})
+});
+
+/*EMAIL JS*/
+function sendForm() {
+    const emailParams = {
+        from_name: document.getElementById("fromName").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value,
+    };
+    
+    emailjs.send('service_h97e6hv', 'template_9zly852', emailParams)
+       .then(function(res) {
+           alert("Thank you for your message!");
+           console.log("Success!", res.status)
+       })
+       .catch(function(res) {
+           alert("Sorry! There was an unexpected error.")
+           console.log("There was an error!", res.status)
+       });
+       
+       document.getElementById("contactForm").reset(); 
+}
+
+
+
+
+
